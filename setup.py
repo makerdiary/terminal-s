f3e75b62c10c077a1816d4768904c79e31a91c0e
Yihui Xiong <yihui.xiong@hotmail.com>
2020-05-28 19:46:03 +0800

    0.1.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup_requirements = ['wheel']
 
 
 setup(name='terminal-s',
-      version='0.1.1',
+      version='0.1.2',
       description='A super simple serial terminal',
       long_description=long_description,
       long_description_content_type='text/markdown',
